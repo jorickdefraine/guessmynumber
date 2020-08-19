31596deb49e53673dfc64b5f718db4a4468480f0
jorickdefraine <defraine.jorick@gmail.com>
2020-08-20 00:36:54 +0200

    add prediction with keras

--- gmnV0.py
+++ gmnV0.py
@@ -7,9 +7,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 from torchvision.transforms import ToTensor
-
+from data.examples.samples import *
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array, load_img
 
 class Net(nn.Module):
     def __init__(self):
@@ -110,12 +112,15 @@ def main():
 
 
 # Process our image
-def process_image(image_path):
+def process_image(img):
     # Load Image
-    img = Image.open(image_path).convert('1')
+    if type(img) == str:
+        img = Image.open(image_path)
 
+    img = img.convert('1')
     # Get the dimensions of the image
     width, height = img.size
+
     # Resize by keeping the aspect ratio, but changing the dimension
     # so the shortest size is 255px
     img = img.resize((28, int(28 * (height / width))) if width < height else (int(28 * (width / height)), 28))
@@ -138,23 +143,31 @@ def process_image(image_path):
 
     # Make all values between 0 and 1
     img = img / 28
-
+    img = (img-0.1307)/0.3081
     # Normalize based on the preset mean and standard deviation
-    img[0] = (img[0] - 0.1307) / 0.3081
-    img[1] = (img[1] - 0.1307) / 0.3081
+    #img[0] = (img[0] - 0.1307) / 0.3081
+    #img[1] = (img[1] - 0.1307) / 0.3081
     #img[2] = (img[2] - 0.1307) / 0.3081
-
     # Add a fourth dimension to the beginning to indicate batch size
-    #img = img[np.newaxis, :]
-    img = img[np.newaxis, np.newaxis, :]
+    # img = img[np.newaxis, :]
+    #new_img = img[np.newaxis, np.newaxis, :]
     # Turn into a torch tensor
-    image = torch.from_numpy(img)
-    image = image.float()
+    image = Image.fromarray(img, '1')
+    #image = torch.from_numpy(img)
+    ##image = image.unsqueeze(0).unsqueeze(0)
+    #image = image.float()
+
     return image
 
 
-def predict(image, network):
-    network = network.eval()
+def predict(image):
+    network = Net()
+
+    network.load_state_dict(torch.load("./results/model.pth"))
+
+    #network = torch.load("./results/model.pth")
+    network.eval()
+    #network = network.eval()
 
     # Pass the image through our model
     output = network(image)
@@ -167,18 +180,60 @@ def predict(image, network):
     probs, classes = output.topk(1, dim=1)
     return probs.item(), classes.item()
 
-if __name__ == '__main__':
-    #main()
-    image_path = "./data/examples/image.png"
-    image = process_image(image_path)
-    network = Net()
-    print(image.shape)
-    network.load_state_dict(torch.load('./results/model.pth'))
-    top_prob, top_class = predict(image, network)
-    print(top_prob, top_class)
-    #model = Net()
-    #model.load_state_dict(torch.load('./results/model.pth'))
 
-    #output = model(input)
-    #prediction = int(torch.max(output.data, 1)[1].numpy())
-    #print(prediction)
+def stringtotensor(input):
+    position = input[14:-2].replace('{', '').replace('}', '').replace('"', '') \
+        .replace(':', '').replace(',', '').replace('x', ' ').replace('y', ' ').split(' ')
+    position = [int(i) for i in position]
+    position = np.array(position).reshape((int(len(position) / 2), 2))
+
+    w, h = 280, 280
+    output = np.zeros((h, w, 3), dtype=np.uint8)
+
+    for element in position:
+        for i in range(30):
+            output[element[0]:element[0]+i,element[1]:element[1]+i] = [255, 255, 255]
+
+
+
+    img = Image.fromarray(output, 'RGB').convert('1')
+    img_mirror = ImageOps.mirror(img)
+    transposed = img_mirror.transpose(Image.ROTATE_90)
+    return transposed
+def predictwithkeras(image_path):
+
+    # Test on the image form test folders
+    img_width, img_height = 28, 28
+    test_model = load_model("./results/conv2D_classifier.h5")
+    img = load_img(image_path, False, target_size=(img_width, img_height), color_mode="grayscale")
+    print(type(img))
+    # img.show()
+    x = img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    preds = test_model.predict_classes(x)
+    prob = test_model.predict_proba(x)
+    prob2 = test_model.predict(x)
+    return prob, preds
+
+if __name__ == '__main__':
+    # main()
+    #image_path = "./data/examples/image.png"
+    ## input = '{"points":[{"x":33,"y":29},{"x":34,"y":29},{"x":34,"y":28},{"x":35,"y":28},{"x":36,"y":28}]}'
+    #image = process_image(image_path)
+    #print(image.shape)
+    ## print(stringtotensor(input))
+    #output = predict(image)
+    #print(output)
+
+    drawingJSON = '{"points":[{"x":100,"y":91},{"x":98,"y":90},{"x":96,"y":90},{"x":95,"y":90},{"x":91,"y":90},{"x":89,"y":90},{"x":85,"y":88},{"x":82,"y":87},{"x":80,"y":86},{"x":77,"y":84},{"x":76,"y":83},{"x":75,"y":81},{"x":74,"y":79},{"x":74,"y":77},{"x":74,"y":73},{"x":75,"y":71},{"x":76,"y":70},{"x":77,"y":68},{"x":79,"y":67},{"x":81,"y":65},{"x":85,"y":63},{"x":89,"y":61},{"x":93,"y":60},{"x":96,"y":60},{"x":100,"y":59},{"x":102,"y":58},{"x":106,"y":58},{"x":109,"y":58},{"x":113,"y":58},{"x":121,"y":60},{"x":124,"y":62},{"x":126,"y":64},{"x":127,"y":66},{"x":128,"y":70},{"x":129,"y":74},{"x":130,"y":80},{"x":130,"y":85},{"x":129,"y":91},{"x":128,"y":96},{"x":125,"y":102},{"x":123,"y":113},{"x":120,"y":119},{"x":118,"y":124},{"x":116,"y":128},{"x":114,"y":131},{"x":112,"y":134},{"x":110,"y":137},{"x":107,"y":141},{"x":104,"y":146},{"x":102,"y":148},{"x":100,"y":149},{"x":99,"y":150},{"x":98,"y":150},{"x":96,"y":150},{"x":95,"y":150},{"x":92,"y":150},{"x":91,"y":150},{"x":90,"y":150},{"x":87,"y":149},{"x":84,"y":147},{"x":80,"y":145},{"x":79,"y":144},{"x":76,"y":144},{"x":74,"y":143},{"x":72,"y":141},{"x":72,"y":140},{"x":71,"y":139},{"x":71,"y":138},{"x":71,"y":136},{"x":73,"y":135},{"x":74,"y":134},{"x":77,"y":134},{"x":80,"y":133},{"x":84,"y":133},{"x":88,"y":133},{"x":93,"y":135},{"x":96,"y":136},{"x":99,"y":138},{"x":101,"y":139},{"x":103,"y":140},{"x":105,"y":141},{"x":106,"y":142},{"x":108,"y":144},{"x":109,"y":144},{"x":111,"y":145},{"x":113,"y":147},{"x":118,"y":149},{"x":121,"y":150},{"x":126,"y":151},{"x":130,"y":152},{"x":135,"y":152},{"x":136,"y":152},{"x":140,"y":152},{"x":147,"y":152},{"x":152,"y":152},{"x":158,"y":152},{"x":163,"y":152},{"x":167,"y":152},{"x":169,"y":152},{"x":170,"y":152},{"x":171,"y":152},{"x":171,"y":151}]}'
+    pre_image = stringtotensor(neuf)
+    pre_image.show()
+    #image = process_image(pre_image)
+    #image.show()
+    pre_image.save('./data/examples/pre_image.png')
+    # print(pre_image)
+    ## print(image)
+    #output = predict(image)
+    #print(output)
+    path = './data/examples/pre_image.png'
+    print(predictwithkeras(path))
---
+++ gmn_keras.py
@@ -0,0 +1,264 @@
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+import sys, os
+import numpy as np
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QFrame, QApplication
+
+import main
+from main import *
+
+
+class predict_worker(object):
+    def __init__(self, parent=None, model_f="model/conv2D_classifier.h5"):
+        self.cur_model = load_model(model_f)
+        self.parent = parent
+
+    def process_data(self, data):
+        self.cur_data = data
+        self.parent.results.setText("Calculating...")
+
+        self.x_pos = self.cur_data.x_pos
+        self.y_pos = self.cur_data.y_pos
+
+        buf = 4  # 4 buffer on all sides
+
+        max_x = max(self.x_pos)
+        min_x = min(self.x_pos)
+        max_y = max(self.y_pos)
+        min_y = min(self.y_pos)
+
+        x_span = float(max_x - min_x)
+        y_span = float(max_y - min_y)
+
+        for i in range(len(self.x_pos)):
+            self.x_pos[i] = (self.x_pos[i] - min_x) / x_span * 20.0 + buf
+            self.y_pos[i] = (self.y_pos[i] - min_y) / y_span * 20.0 + buf
+
+        pixels = []
+
+        for y in range(28):
+            for x in range(28):
+                f_len = len(pixels)
+                for p_x, p_y in zip(self.x_pos, self.y_pos):
+                    if int(p_x) in [x - 1, x, x + 1] and int(p_y) in [y - 1, y, y + 1]:
+                        pixels.append(1.0)
+                        break
+                if f_len == len(pixels):
+                    pixels.append(0.0)
+
+        image = image_t(pixels)
+        image.output_terminal(threshold=0.9)
+
+        if K.image_data_format() == 'channels_first':
+            input_shape = (1, 28, 28)
+        else:
+            input_shape = (28, 28, 1)
+
+        image = np.array([np.array(image.pixels).reshape(input_shape)])
+        prediction = self.cur_model.predict(image, batch_size=1, verbose=0)
+
+        highest_prob = 0.0
+        highest_prob_val = None
+
+        i = 0
+        for p in prediction[0]:
+            if p > highest_prob:
+                highest_prob = p
+                highest_prob_val = i
+            i += 1
+
+        self.parent.results.setText("Digit is " + str(highest_prob_val) + ", prob = %0.2f" % (100.0 * highest_prob))
+
+
+class drawing_path():
+    def __init__(self):
+        self.x_pos = []
+        self.y_pos = []
+
+    def add_point(self, x, y):
+        self.x_pos.append(x)
+        self.y_pos.append(y)
+
+    def clear_path(self):
+        self.x_pos = []
+        self.y_pos = []
+
+    def print_path(self):
+        smallest_x = 1000
+        largest_x = 0
+        smallest_y = 1000
+        largest_y = 0
+
+        for x, y in list(zip(self.x_pos, self.y_pos)):
+            if x > largest_x:
+                largest_x = x
+            if x < smallest_x:
+                smallest_x = x
+            if y > largest_y:
+                largest_y = y
+            if y < smallest_y:
+                smallest_y = y
+
+        translated_x = []
+        translated_y = []
+
+        for x, y in list(zip(self.x_pos, self.y_pos)):
+            translated_x.append(x - smallest_x)
+            translated_y.append(y - smallest_y)
+
+        x_size = largest_x - smallest_x
+        y_size = largest_y - smallest_y
+
+        for y in range(y_size):
+            line = ""
+            for x in range(x_size):
+                isPixel = False
+                for x_coor, y_coor in list(zip(translated_x, translated_y)):
+                    if isPixel == False:
+                        if x_coor == x and y_coor == y:
+                            line += "X"
+                            isPixel = True
+                if isPixel == False:
+                    line += "  "
+            print(line)
+
+
+class window(QWidget):
+    def __init__(self, parent=None):
+        super(window, self).__init__()
+        self.worker = predict_worker(parent=self)
+        self.init_ui()
+
+    def init_ui(self):
+        self.setFixedHeight(600)
+        self.setFixedWidth(450)
+        self.setWindowTitle("MNIST Digit Prediction")
+        self.hasDrawing = False
+        self.mouseHeld = False
+
+        self.path = drawing_path()
+
+        self.rect = QRect(0, 50, 400, 400)
+
+        self.label = QLabel("Click and hold the left mouse button to draw.", self)
+        self.label.move(25, 10)
+        self.label.setFixedWidth(300)
+
+        self.label2 = QLabel("Classifications include numerals (0-9).", self)
+        self.label2.move(25, 35)
+        self.label2.setFixedWidth(300)
+
+        self.results = QLabel("Results will appear here", self)
+        self.results.move(25, 540)
+        self.results.setFixedWidth(300)
+        self.result_label = QLabel("", self)
+        self.result_label.move(330, 490)
+
+        self.clear_button = QPushButton("Clear", self)
+        self.clear_button.move(330, 535)
+        self.clear_button.clicked.connect(self.clear)
+
+        self.upper_line = QFrame(self)
+        self.upper_line.setFrameShape(QFrame.HLine)
+        self.upper_line.move(25, 85)
+        self.upper_line.setFixedWidth(400)
+
+        self.lower_line = QFrame(self)
+        self.lower_line.setFrameShape(QFrame.HLine)
+        self.lower_line.move(25, 485)
+        self.lower_line.setFixedWidth(400)
+
+        self.left_line = QFrame(self)
+        self.left_line.setFrameShape(QFrame.VLine)
+        self.left_line.move(-25, 100)
+        self.left_line.setFixedHeight(400)
+
+        self.right_line = QFrame(self)
+        self.right_line.setFrameShape(QFrame.VLine)
+        self.right_line.move(375, 100)
+        self.right_line.setFixedHeight(400)
+
+        self.show()
+
+    def clear(self):
+        self.path.clear_path()
+        self.update()
+
+    def mousePressEvent(self, event):
+        x = event.x()
+        y = event.y()
+        self.path.clear_path()
+
+        if 100 < y < 500:
+            if 25 < x < 425:
+                if self.hasDrawing == True:
+                    self.path.clear()
+                self.mouseHeld = True
+
+                position = event.pos()
+
+                self.path.add_point(x, y)
+
+                self.results.setText("Position = " + str(position))
+                return
+            else:
+                self.results.setText("Position out of range")
+                self.mouseHeld = False
+                return
+        self.mouseHeld = False
+        self.results.setText("Position out of range")
+        return
+
+    def mouseMoveEvent(self, event):
+        x = event.x()
+        y = event.y()
+        if 100 < y < 500:
+            if 25 < x < 425:
+                if self.mouseHeld == True:
+                    position = event.pos()
+                    self.path.add_point(x, y)
+                    self.results.setText("Position = " + str(position))
+                    self.update()
+                return
+            else:
+                self.results.setText("Position out of range")
+        else:
+            self.results.setText("Position out of range")
+
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+
+        last_x = 0
+        last_y = 0
+        for x, y in list(zip(self.path.x_pos, self.path.y_pos)):
+            if last_x == 0:
+                last_x = x
+                last_y = y
+            else:
+                painter.drawLine(last_x, last_y, x, y)
+                last_x = x
+                last_y = y
+        # painter.drawLine(self.last_x, self.last_y, self.cur_x, self.cur_y)
+        painter.end()
+
+    def mouseReleaseEvent(self, event):
+        self.mouseHeld = False
+        if len(self.path.x_pos) < 4: return
+        self.results.setText("Processing Data...")
+        self.worker.process_data(self.path)
+
+    def update_label(self, text):
+        self.results.setText(text)
+
+
+def main():
+    app = QApplication(sys.argv)
+    _ = window()
+    sys.exit(app.exec_())
+
+
+#if __name__ == '__main__':
+#    main()
